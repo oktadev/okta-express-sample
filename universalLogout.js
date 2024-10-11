535e61bf5d08ce26a0c597c3b0bf0df1dd430d1e
Sara Kazemi <sara.kazemi@okta.com>
2024-10-11 16:34:05 -0700

    Update universalLogout.js

--- universalLogout.js
+++ universalLogout.js
@@ -1,9 +1,39 @@
 var express = require('express');
 const universalLogoutRoute = express.Router();
 const store  = require('./sessionStore');
-
+var OktaJwtVerifier = require('@okta/jwt-verifier');
 
 universalLogoutRoute.post('/global-token-revocation', async (req, res) => {
+
+  // JWT Verification
+  // const oktaJwtVerifier = new OktaJwtVerifier({
+  //   issuer: `${ORG_URL}`,
+  //   jwksUri: `${ORG_URL}/oauth2/v1/keys`,
+  // });
+
+  // const tokenValidator = async function (req, res, next) {
+  //   const authHeaders = req.headers.authorization;
+  //   if (!authHeaders) {
+  //     return res.sendStatus(401);
+  //   }
+  //   const parts = authHeaders.split(' ');
+  //   const jwt = parts[1];
+  //   const expectedAud =
+  //     `https://${codespaceName}-3000.app.github.dev/global-token-revocation`;
+  
+  //   try {
+  //     const verifiedJwt = await oktaJwtVerifier.verifyAccessToken(
+  //       jwt,
+  //       expectedAud
+  //     );
+  //     console.log(verifiedJwt.claims);
+  //   } catch (err) {
+  //     console.log(err);
+  //     return res.sendStatus(401);
+  //   }
+  // };
+
+
   // 204 When the request is successful
   const httpStatus = 204;
 
@@ -42,6 +72,5 @@ universalLogoutRoute.use((err,req,res,next) => {
     return res.sendStatus(404)
   }
 })
-
 
 module.exports = universalLogoutRoute;
